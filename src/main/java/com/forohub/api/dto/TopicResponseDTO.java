d1db8fa7c6b59d7a68dc87be72559cbe959a0909
Jilmar-Perello <jilmarjoancy@gmail.com>
2026-01-27 21:18:08 -0600

    Crear DTO y validaciones

---
+++ src/main/java/com/forohub/api/dto/TopicResponseDTO.java
@@ -0,0 +1,18 @@
+package com.forohub.api.dto;
+
+import com.forohub.api.entity.Topic;
+
+import java.time.LocalDateTime;
+
+public record TopicResponseDTO(
+        Long id,
+        String titulo,
+        String mensaje,
+        LocalDateTime fechaCreacion
+) {
+    public TopicResponseDTO (Topic topico){
+        this(
+            topico.getId(), topico.getTitulo(), topico.getMensaje(), topico.getFechaCreacion()
+        );
+    }
+}
